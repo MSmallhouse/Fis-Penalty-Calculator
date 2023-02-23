214d4117591ce268a36b26179060161092fd7b2a
Matt Smallhouse <smallhou@bc.edu>
2023-02-23 18:45:24 -0500

    works for finding points given a list of names

--- get_points_by_csv.py
+++ get_points_by_csv.py
@@ -1,17 +1,14 @@
 # get points by list for faster searching
 import pandas as pd
-# MS FILTER FIRST AND LAST NAMES TO GET RID OF CAPITALS
-first = "Matthew"
-last = "smallhouse"
 
-first = first.lower()
-last = last.lower()
+first_names = ["Matthew", "Ben"]
+last_names = ["Smallhouse", "Charleston"]
 
 points_list = pd.read_csv("FIS-points-list-AL-2023-363.csv")
 info = points_list[["Lastname", "Firstname", "SLpoints", "GSpoints"]]
-info["Lastname"] = info["Lastname"].apply(str.lower)
-info["Firstname"] = info["Firstname"].apply(str.lower)
 
-my_info = info[(info["Lastname"] == last) & (info["Firstname"] == first)]
 
-print(my_info)
+for first, last in zip(first_names, last_names):
+    my_info = info[(info["Lastname"].str.lower() == last.lower()) & (info["Firstname"].str.lower() == first.lower())]
+
+    print(my_info)
