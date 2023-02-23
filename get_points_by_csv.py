a34942129e06fa7b41c763b23e4cd0000b8f186f
Matt Smallhouse <smallhou@bc.edu>
2023-02-23 15:58:08 -0500

    get one person's points given first, last name

--- get_points_by_csv.py
+++ get_points_by_csv.py
@@ -1,2 +1,17 @@
 # get points by list for faster searching
-# comment
+import pandas as pd
+# MS FILTER FIRST AND LAST NAMES TO GET RID OF CAPITALS
+first = "Matthew"
+last = "smallhouse"
+
+first = first.lower()
+last = last.lower()
+
+points_list = pd.read_csv("FIS-points-list-AL-2023-363.csv")
+info = points_list[["Lastname", "Firstname", "SLpoints", "GSpoints"]]
+info["Lastname"] = info["Lastname"].apply(str.lower)
+info["Firstname"] = info["Firstname"].apply(str.lower)
+
+my_info = info[(info["Lastname"] == last) & (info["Firstname"] == first)]
+
+print(my_info)
