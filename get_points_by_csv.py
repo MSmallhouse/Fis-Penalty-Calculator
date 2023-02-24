638c5b3dd73639248e5542e870f527ae517d7dbd
Matt Smallhouse <smallhou@bc.edu>
2023-02-23 19:02:53 -0500

    renamed some stuff

--- get_points_by_csv.py
+++ get_points_by_csv.py
@@ -7,8 +7,9 @@ last_names = ["Smallhouse", "Charleston"]
 points_list = pd.read_csv("FIS-points-list-AL-2023-363.csv")
 info = points_list[["Lastname", "Firstname", "SLpoints", "GSpoints"]]
 
-
+people = pd.DataFrame()
 for first, last in zip(first_names, last_names):
-    my_info = info[(info["Lastname"].str.lower() == last.lower()) & (info["Firstname"].str.lower() == first.lower())]
+    new_info = info[(info["Lastname"].str.lower() == last.lower()) & (info["Firstname"].str.lower() == first.lower())]
+    people = pd.concat([people, new_info])
 
-    print(my_info)
+print(people)
